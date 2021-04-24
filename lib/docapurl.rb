aca4870b501ded117f4f4af4585b61beceec854c
atpking <atpking@gmail.com>
2021-04-24 15:47:54 +0800

    fix

--- lib/docapurl.rb
+++ lib/docapurl.rb
@@ -9,11 +9,11 @@ module Docapurl
   # Your code goes here...
   class Cli < Thor
     desc 'cap [url]', 'cap url'
-    method_options browser_path: :string
-    method_options max_pagedown: :numeric, default: 5
-    method_options sleep_before_screen: :numeric, default: 0
-    method_options xvfb: false
-    method_options headless: true
+    method_options browser_path: :string, desc: 'specify chrome browser path'
+    method_options max_pagedown: :numeric, default: 5, desc: 'max page down press when page height overs the viewport height'
+    method_options sleep_before_screen: :numeric, default: 0, desc: 'sleep second before_screen'
+    method_options xvfb: false, desc: 'xvfb needed'
+    method_options headless: true, desc: 'run in headless'
     def cap(url, path = nil)
       browser_options = {
         xvfb: options.xvfb,
